a4104e4bdd13d73f4044c0dddd146d03f3b7768d
Mariano Loza <marianoloza68@gmail.com>
2023-09-20 22:19:48 -0300

    proyects section almost ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Proyects from "./components/Proyects";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Navbar />
     <Home />
     <About />
+    <Proyects />
 
     <SocialLinks />
    </div>
---
+++ src/components/Proyects.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import DreamLodge from "../assets/proyects/DreamLodge.png"
+import PerriPedia from "../assets/proyects/PerriPedia.png"
+
+const Proyects = () => {
+
+    const proyects = [
+        {
+            id: 1,
+            src: DreamLodge
+        },
+        {
+            id: 2,
+            src: PerriPedia
+        }
+    ]
+
+  return (
+  <div name="proyects" 
+       className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+
+    <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Proyects</p>
+            <p className='py-6'>Check out some of my work right here</p>
+        </div>
+
+        <div className='grid sm:grid-cols-2 gap-8 px-12 sm:px-0'>
+        {
+            proyects.map(({id, src}) => (
+            <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                <img src={src} 
+                     alt="" 
+                     className='rounded-md duration-200 hover:scale-105'/>
+                <div className='flex items-center justify-center'>
+                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                </div>
+            </div>
+            ))
+        }
+        </div>     
+
+    </div>
+
+  </div>
+  )
+}
+
+export default Proyects
